9eee7dd859dbc2222df093f9fca63a254560c9ed
Adrian Inza-Cruz <ainzacruz@gmail.com>
2021-03-22 16:54:29 -0700

    Add files via upload

---
+++ src/gif/Pmetrics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Gif from './giphy.gif'
+
+const Pmetrics = () => {
+  return (
+    <div>
+      <img src={Gif} alt="cat"/>
+    </div>
+  );
+};
+
+export default Pmetrics;
---
+++ src/pages/index.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import Button from '../components/Button';
+import Card from '../components/Card';
+import CustomerCard from '../components/CustomerCard';
+import LabelText from '../components/LabelText';
+import Layout from '../components/layout/Layout';
+import SplitSection from '../components/SplitSection';
+import StatsBox from '../components/StatsBox';
+import customerData from '../data/customer-data';
+import HeroImage from '../svg/HeroImage';
+import Pmetrics from '../gif/Pmetrics'
+
+export default () => (
+  <Layout>
+    <section className="pt-20 md:pt-40">
+      <div className="container mx-auto px-8 lg:flex">
+        <div className="text-center lg:text-left lg:w-1/2">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+            JanusQL
+          </h1>
+          <p className="text-xl lg:text-2xl mt-6 font-light">
+          A tool for GraphQL API security and performance testing
+          </p>
+          <p className="mt-8 md:mt-12">
+            <Button size="lg">Download</Button>
+          </p>
+        </div>
+        <div className="lg:w-1/2">
+          <HeroImage />
+        </div>
+      </div>
+    </section>
+    <section id="features" className="py-20 lg:pb-40 lg:pt-48">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
+        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Intuitive GUI</p>
+              <p className="mt-4">
+                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                velna vitae auctor integer.
+              </p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Performance Metrics</p>
+              <p className="mt-4">
+                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                velna vitae auctor integer.
+              </p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Security Metrics</p>
+              <p className="mt-4">
+                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                velna vitae auctor integer.
+              </p>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+    <SplitSection
+      id="services"
+      primarySlot={
+        <div className="lg:pr-32 xl:pr-48">
+          <h3 className="text-3xl font-semibold leading-tight">Intuitive GUI Playground</h3>
+          <p className="mt-8 text-xl font-light leading-relaxed">
+            GUI Playground
+          </p>
+        </div>
+      }
+      secondarySlot={<Pmetrics />}
+    />
+    <SplitSection
+      reverseOrder
+      primarySlot={
+        <div className="lg:pl-32 xl:pl-48">
+          <h3 className="text-3xl font-semibold leading-tight">
+            Performance Metrics
+          </h3>
+          <p className="mt-8 text-xl font-light leading-relaxed">
+            Performance Metrics
+          </p>
+        </div>
+      }
+      secondarySlot={<Pmetrics />}
+    />
+    <SplitSection
+      primarySlot={
+        <div className="lg:pr-32 xl:pr-48">
+          <h3 className="text-3xl font-semibold leading-tight">
+            Security Metrics
+          </h3>
+          <p className="mt-8 text-xl font-light leading-relaxed">
+            Security Metrics
+          </p>
+        </div>
+      }
+      secondarySlot={<Pmetrics />}
+    />
+    <section id="stats" className="py-20 lg:pt-32">
+      <div className="container mx-auto text-center">
+        <LabelText className="text-gray-600">Our Team</LabelText>
+        <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="Phillip" secondaryText="Bannister" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="Lucas" secondaryText="Mobley" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="Kim" secondaryText="Chiu" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="Tammy" secondaryText="Le" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="Adrian" secondaryText="Inza-Cruz" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
+      <h3 className="text-5xl font-semibold">Try JanusQL Today</h3>
+      <p className="mt-8 text-xl font-light">
+        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+      </p>
+      <p className="mt-8">
+        <Button size="xl">Download</Button>
+      </p>
+    </section>
+  </Layout>
+);
